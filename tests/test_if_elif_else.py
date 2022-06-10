43d45c88b00c5a6e751a5ab08a6e03e1a67bcfc8
Jason Lau <lau@cs.ucla.edu>
2022-06-09 19:31:27 -0700

    test: add examples to tests

---
+++ tests/test_if_elif_else.py
@@ -0,0 +1,33 @@
+import unittest
+
+from dice import dice, sample
+import random
+
+def evaluate():
+    a = random.choices([True, False], weights=[4, 6])[0]
+    b = random.choices([True, False], weights=[3, 7])[0]
+    if a:
+        c = random.choices([True, False], weights=[2, 8])[0]
+    elif b:
+        c = random.choices([True, False], weights=[1, 9])[0]
+    else:
+        c = random.choices([True, False], weights=[3, 7])[0]
+    result = a and b and c
+    if result:
+        return c
+    return result
+
+class TestIfElifElse(unittest.TestCase):
+
+    def test_single_dice(self):
+        result = dice()(evaluate)()
+        self.assertAlmostEqual(result[True], 0.025)
+        self.assertAlmostEqual(result[False], 0.975)
+
+    def test_single_sample(self):
+        result = sample(100000)(evaluate)()
+        self.assertAlmostEqual(result[True], 0.025, 1)
+        self.assertAlmostEqual(result[False], 0.975, 1)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_if_else.py
@@ -0,0 +1,30 @@
+import unittest
+
+from dice import dice, sample
+import random
+
+def evaluate():
+    b = random.choices([True, False], weights=[3, 7])[0]
+    if b:
+        a = random.choices([True, False], weights=[3, 7])[0]
+    else:
+        a = random.choices([True, False], weights=[2, 8])[0]
+    result = b or a
+    if result:
+        return b
+    return result
+
+class TestIfElse(unittest.TestCase):
+
+    def test_single_dice(self):
+        result = dice()(evaluate)()
+        self.assertAlmostEqual(result[True], 0.3)
+        self.assertAlmostEqual(result[False], 0.7)
+
+    def test_single_sample(self):
+        result = sample(100000)(evaluate)()
+        self.assertAlmostEqual(result[True], 0.3, 1)
+        self.assertAlmostEqual(result[False], 0.7, 1)
+
+if __name__ == '__main__':
+    unittest.main()
